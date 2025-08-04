b8e3957fb630bcae71be9a7d02b9b8b0c70cafc0
jayasonicas <jayasonica.240125@aids.ritchennai.edu.in>
2025-08-04 14:41:10 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+package exercise;
+
+public class Fibonacci {
+
+    
+    public static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;  
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
