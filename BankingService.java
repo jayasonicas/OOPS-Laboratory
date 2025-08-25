9f2edec5145a260faa91ca95d474048b55214332
jayasonicas <jayasonica.240125@aids.ritchennai.edu.in>
2025-08-25 14:09:01 +0530

    Create BankingService.java

---
+++ BankingService.java
@@ -0,0 +1,59 @@
+interface BankingService {
+    void deposit(double amount);
+    void withdraw(double amount);
+    void showBalance();
+}
+
+
+class Account implements BankingService {
+    private double balance;
+
+    public Account() {
+        this.balance = 0.0;
+    }
+
+    private String formatAmount(double amount) {
+       
+        if (amount == (int) amount) {
+            return String.valueOf((int) amount);
+        } else {
+            return String.valueOf(amount);
+        }
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit ₹" + formatAmount(amount) + " → Balance = ₹" + formatAmount(balance));
+        } else {
+            System.out.println("Deposit ₹" + formatAmount(amount) + " → Output: Invalid");
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            System.out.println("Withdraw ₹" + formatAmount(amount) + " from ₹" 
+                               + formatAmount(balance) + " → Balance = ₹" + formatAmount(balance - amount));
+            balance -= amount;
+        } else if (amount > balance) {
+            System.out.println("Withdraw ₹" + formatAmount(amount) + " from ₹" 
+                               + formatAmount(balance) + " → Output: Insufficient funds");
+        } else {
+            System.out.println("Withdraw ₹" + formatAmount(amount) + " → Output: Invalid");
+        }
+    }
+
+    @Override
+    public void showBalance() {
+        System.out.println("Current Balance = ₹" + formatAmount(balance));
+    }
+}
+
+
+class Transaction {
+    public void record(String action) {
+   
+    }
+}
