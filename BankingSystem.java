e5556cf82749d77661f42bcb38d1d76d55a573de
jayasonicas <jayasonica.240125@aids.ritchennai.edu.in>
2025-08-25 14:09:42 +0530

    Create BankingSystem.java

---
+++ BankingSystem.java
@@ -0,0 +1,20 @@
+public class BankingSystem {
+    public static void main(String[] args) {
+        BankingService account = new Account();  // Polymorphism
+
+        // TC1: Deposit ₹1000
+        account.deposit(1000);
+
+        // TC2: Withdraw ₹500 from ₹1000
+        account.withdraw(500);
+
+        // TC3: Withdraw ₹1500 from ₹500
+        account.withdraw(1500);
+
+        // TC4: Deposit negative amount
+        account.deposit(-200);
+
+        // TC5: Just show balance
+        account.showBalance();
+    }
+}
